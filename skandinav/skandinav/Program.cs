0c97f31837732dbacb53b2655a162ef39d4a93a4
JazzyG99 <144050370+JazzyG99@users.noreply.github.com>
2025-01-17 11:00:33 +0100

    Add files via upload

---
+++ skandinav/skandinav/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace skandianvlotto
+{
+    public class Program
+    {
+
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            //lotto(5, 90);
+            //lotto(6, 45);
+            //lotto(7, 35);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+
+
+            //Console.ReadKey();
+
+        }
+    }
+}
